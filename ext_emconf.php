7b22bb3acdf74bcc35fdf95549c6ba95a013a3f9
Sebastian Fischer <sf@marketing-factory.de>
2013-05-08 15:45:43 +0000

    Fix authentication

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "authagainsttypo3"
+# Extension Manager/Repository config file for ext "authagainsttypo3".
 #
-# Auto generated 03-11-2009 14:48
+# Auto generated 17-02-2012 16:59
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'Marketing Factory Consulting GmbH',
-	'version' => '0.9.2',
+	'version' => '0.10.1',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"60b0";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"8f26";s:14:"ext_tables.php";s:4:"d10d";s:16:"locallang_db.xml";s:4:"c608";s:14:"doc/manual.sxw";s:4:"d615";s:37:"pi1/class.tx_authagainsttypo3_pi1.php";s:4:"b21b";s:17:"pi1/locallang.xml";s:4:"bfa9";s:24:"pi1/static/editorcfg.txt";s:4:"2f67";s:31:"static/Webservice/constants.txt";s:4:"790a";s:27:"static/Webservice/setup.txt";s:4:"3d72";s:21:"contrib/post-test.php";s:4:"6484";}',
+	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"79bc";s:12:"ext_icon.gif";s:4:"27aa";s:17:"ext_localconf.php";s:4:"9768";s:14:"ext_tables.php";s:4:"d10d";s:16:"locallang_db.xml";s:4:"c608";s:23:"cli/class.cliAuthBE.php";s:4:"3251";s:21:"contrib/post-test.php";s:4:"6484";s:14:"doc/manual.sxw";s:4:"d615";s:37:"pi1/class.tx_authagainsttypo3_pi1.php";s:4:"b21b";s:17:"pi1/locallang.xml";s:4:"bfa9";s:24:"pi1/static/editorcfg.txt";s:4:"2f67";s:31:"static/Webservice/constants.txt";s:4:"790a";s:27:"static/Webservice/setup.txt";s:4:"3d72";}',
 	'suggests' => array(
 	),
 );
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,4 +1,6 @@
 <?php
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 t3lib_extMgm::addPItoST43($_EXTKEY,'pi1/class.tx_authagainsttypo3_pi1.php','_pi1','list_type',1);
+
+$TYPO3_CONF_VARS['SC_OPTIONS']['GLOBAL']['cliKeys'][$_EXTKEY] = array('EXT:' . $_EXTKEY . '/cli/class.cliAuthBE.php', '_CLI_user');
 ?>
