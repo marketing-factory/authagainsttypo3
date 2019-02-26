2fd6436ed90c32404e966d2446982b63000f572a
Christian Spoo <cs@marketing-factory.de>
2019-02-26 14:11:40 +0100

    Add compatibility with TYPO3 9.5

---
+++ Classes/Command/AuthenticationLoginCommand.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+namespace Mfc\Authagainsttypo3\Command;
+
+use Mfc\Authagainsttypo3\Services\CliAuthenticationService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * Class AuthenticationLoginCommand
+ * @package Mfc\Authagainsttypo3\Command
+ * @author Christian Spoo <cs@marketing-factory.de>
+ */
+class AuthenticationLoginCommand extends Command
+{
+    /**
+     * @var CliAuthenticationService
+     */
+    private $cliAuthenticationService;
+
+    public function __construct(?string $name = null)
+    {
+        parent::__construct($name);
+
+        $this->cliAuthenticationService = GeneralUtility::makeInstance(CliAuthenticationService::class);
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        try {
+            $user = (string)getenv('USER');
+            $password = (string)getenv('PASSWORD');
+
+            if ($this->cliAuthenticationService->authenticateUser($user, $password)) {
+                exit(0);
+            }
+        } finally {
+            exit(1);
+        }
+    }
+}
--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF['authagainsttypo3'] = array(
 
     'constraints' => array(
         'depends' => array(
-            'typo3' => '8.7.0-8.99.99',
+            'typo3' => '8.7.0-9.5.99',
         ),
         'conflicts' => array(),
         'suggests' => array(),
